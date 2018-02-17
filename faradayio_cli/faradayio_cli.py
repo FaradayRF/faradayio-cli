586e0b2da8073cef034a85bc7dffa05bd07d4ff5
kb1lqc <Bryce@FaradayRF.com>
2018-02-17 09:38:06 -0800

    Initial argparse implementation
    * Imported argparse module
    * Implemented setupArgParse()

--- faradayio_cli/faradayio_cli.py
+++ faradayio_cli/faradayio_cli.py
@@ -3,8 +3,16 @@
 __version__ = "0.0.0"
 
 import sys
+import argparse
 
 from faradayio.faraday import Faraday
 
+def setupArgParse():
+    parser = argparse.ArgumentParser()
+    return parser.parse_args()
+
 def main():
     print("Executing faradayio-cli version {0}".format(__version__))
+
+    # Setup command line arguments
+    args = setupArgParse()
