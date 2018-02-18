dee25c729a51f82898161bf0434155a32d0690c9
kb1lqc <Bryce@FaradayRF.com>
2018-02-18 15:21:17 -0800

    Implemented setupSerialPort()
    * Added new function with logic to detect command line loopback setting
    * Sets up a loopback test serial port using SerialTestClass() if `-l` or
    `--loopback` are specified
    * Sets up a normal serial port connection with `-p` or `--port` value
    otherwise.

--- faradayio_cli/faradayio_cli.py
+++ faradayio_cli/faradayio_cli.py
@@ -4,8 +4,10 @@ __version__ = "0.0.0"
 
 import sys
 import argparse
+import serial
 
 from faradayio.faraday import Faraday
+from faradayio.faraday import SerialTestClass
 
 def setupArgparse():
     parser = argparse.ArgumentParser()
@@ -16,10 +18,20 @@ def setupArgparse():
 
     # Optional arguments
     parser.add_argument("-l", "--loopback", action="store_true", help="Use software loopback serial port")
-    parser.add_argument("-p", "--port", default="dev/ttyUSB0", help="Physical serial port of radio")
+    parser.add_argument("-p", "--port", default="/dev/ttyUSB0", help="Physical serial port of radio")
 
     # Parse and return arguments
     return parser.parse_args()
+
+def setupSerialPort(loopback, port):
+    if loopback:
+        # Implement loopback software serial port
+        serialPort = SerialTestClass()
+    else:
+        # TODO enable serial port command line options (keep simple for user!)
+        serialPort = serial.Serial(port, 115200, timeout=1)
+
+    return serialPort
 
 def main():
     print("Executing faradayio-cli version {0}".format(__version__))
@@ -27,6 +39,5 @@ def main():
     # Setup command line arguments
     args = setupArgparse()
 
-    print(args)
     # Setup serial port
-    # setupSerialPort(args.loopback)
+    serialPort = setupSerialPort(args.loopback, args.port)
