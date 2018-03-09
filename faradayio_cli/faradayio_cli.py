bc80c7b29a100116cd780f9624089e1a8e7aba95
kb1lqc <Bryce@FaradayRF.com>
2018-03-08 23:49:41 -0800

    Added ip address `-i' and `--addr` option per #29

--- faradayio_cli/faradayio_cli.py
+++ faradayio_cli/faradayio_cli.py
@@ -35,6 +35,8 @@ def setupArgparse():
                         help="Physical serial port of radio")
     parser.add_argument("-m", "--mtu", default="1500",
                         help="Set Maximum Transmission Unit (MTU)")
+    parser.add_argument("-i", "--addr", default="10.0.0.1",
+                        help="Set IP Address of TUN adapter (Farday Radio)")
 
     # Parse and return arguments
     return parser.parse_args()
